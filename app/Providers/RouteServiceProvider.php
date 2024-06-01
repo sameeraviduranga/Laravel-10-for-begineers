c208883ba27ea1517a06d3609b678357b6549dc4
unknown <sameeraviduranga74@gmail.com>
2024-06-01 11:26:14 +0530

    add com

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Doctrine\Inflector\Rules\Pattern;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -26,6 +27,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Route::pattern('id','[0-9]+');
+        Parent::boot();
         $this->configureRateLimiting();
 
         $this->routes(function () {
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
+use SebastianBergmann\CodeUnit\FunctionUnit;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,22 +14,48 @@ use App\Http\Controllers;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::view('/','INDEX');
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/exe',function(){
+    return "exe ";
+});
+
+Route::get('User/{id}',function($id){
+    return "id - " . $id;
+});
+
+Route::get('Item/{id}',function($id){
+    return "Item id - " . $id;
+});
+
+
 Route::get('ID/{id}',function($id){
     echo 'ID : ' . $id;
-});
+})->where('id','[0-9]+');
 
 Route::get('user/{name?}',function($name ='Tutorial'){
     return $name;
-});
+})->where('name','[a-zA-Z]+');
+
+Route::get('user/{id}/{name?}',function($name ='Tutorial'){
+    return $name;
+})->where('name','[a-zA-Z]+');
 
 Route::get('role',[
     'middleware' => 'Role:editor',
     'uses' => 'TestController@index',
  ]);
+
+ Route::match(['POST','GET'],'/getpost',function(){
+    return "get and post";
+ });
+
+Route::any("/dir",function(){
+    return redirect("/redirected");
+});
  
+
  
